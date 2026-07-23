6ad987e507005bd33a62b5a494ff4b0b1380476b
Pavel Turapin <paveltrpn@integra-s.com>
2026-07-23 09:20:15 +0400

    add render submodule

---
+++ modules/src/main/kotlin/render/Render.kt
@@ -0,0 +1,28 @@
+package tire
+
+import org.lwjgl.glfw.*
+import org.lwjgl.glfw.GLFW.*
+import org.lwjgl.opengl.GL.createCapabilities
+import org.lwjgl.opengl.GL46.*
+import org.lwjgl.system.Configuration
+import org.lwjgl.system.MemoryUtil.*
+
+class Render {
+    private var _run: Boolean
+
+    init {
+        _run = true
+    }
+
+    fun preLoop() -> Unit {
+
+    }
+
+    fun loop() -> Unit {
+
+    }
+
+    fun postLoop() -> Unit {
+
+    }
+}

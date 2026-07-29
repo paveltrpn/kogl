6b75ec79284949ce5b2296727a71313b20a244dc
Pavel Turapin <paveltrpn@integra-s.com>
2026-07-29 16:04:42 +0400

    update

--- modules/src/main/kotlin/render/ShaderSource.kt
+++ modules/src/main/kotlin/render/ShaderSource.kt
@@ -2,14 +2,14 @@ package render
 
 import config.Config
 
-class ShaderHalper(val programName: String) {
+class ShaderSource(val programName: String) {
     // Shader files list with corresponding stage types.
     private var _shadersList: MutableList<Pair<String, ShaderStageType>> = mutableListOf()
 
     init {
 
     }
-    
+
     var shadersList: MutableList<Pair<String, ShaderStageType>> = mutableListOf()
     fun get(): MutableList<Pair<String, ShaderStageType>> {
         return _shadersList

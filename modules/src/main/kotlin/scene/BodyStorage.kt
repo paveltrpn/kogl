f942423646464cd8d68de41709cc201bae8903c7
Pavel Turapin <paveltrpn@mail.ru>
2026-08-04 16:21:14 +0400

    make BodyStorage singleton

--- modules/src/main/kotlin/scene/BodyStorage.kt
+++ modules/src/main/kotlin/scene/BodyStorage.kt
@@ -1,5 +1,28 @@
 package scene
 
-class BodyStorage {
+import mesh.*
 
+class BodyStorage private constructor(storagePath: String) {
+    private var _storage: HashMap<String, Mesh>? = null
+
+    companion object {
+        @Volatile
+        private var instance: BodyStorage? = null
+
+        fun init(storagePath: String): BodyStorage {
+            return instance ?: synchronized(this) {
+                instance ?: BodyStorage(storagePath).also { instance = it }
+            }
+        }
+
+        fun instance(): BodyStorage {
+            return instance ?: throw IllegalStateException(
+                "BodyStorage must be initialized by calling init() first."
+            )
+        }
+    }
+
+    init {
+
+    }
 }

b45ee0581d71ab62a00f36167f10c60446445795
Pavel Turapin <paveltrpn@mail.ru>
2026-08-04 16:43:25 +0400

    add body storage assets

--- modules/src/main/kotlin/mesh/Obj.kt
+++ modules/src/main/kotlin/mesh/Obj.kt
@@ -4,10 +4,10 @@ import kotlin.io.path.Path
 import kotlin.io.path.exists
 import java.io.FileNotFoundException
 
-fun reaadObjFile(path: String): Mesh {
-    val file = Path(path)
-    if (!file.exists()) {
-        throw FileNotFoundException("object file not found at path \"${path}\"")
-    }
-    return Mesh()
-}
+//fun reaadObjFile(path: String): Mesh {
+//    val file = Path(path)
+//    if (!file.exists()) {
+//        throw FileNotFoundException("object file not found at path \"${path}\"")
+//    }
+//    return Mesh()
+//}

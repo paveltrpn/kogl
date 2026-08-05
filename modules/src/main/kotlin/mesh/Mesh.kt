687427d0680096ecc7c6d502b47ae03e026d9392
Pavel Turapin <paveltrpn@mail.ru>
2026-08-05 10:19:25 +0400

    translate cpp code into kotlin

--- modules/src/main/kotlin/mesh/Mesh.kt
+++ modules/src/main/kotlin/mesh/Mesh.kt
@@ -1,25 +1,189 @@
 package mesh
 
-// Dual-Indexing (Per-Face Normals/UVs).
-// Compatible with .obj, FBX, glTF.
+import algebra.Vector2
+import algebra.Vector3
+
+typealias MeshValueType = Float
+
 class ObjTriangleIndices {
-    var vertexIndex = IntArray(3) { it -> 0 }
-    var normalIndex = IntArray(3) { it -> 0 }
-    var texcrdIndex = IntArray(3) { it -> 0 }
-};
+    var vertexIndex = IntArray(3) { 0 }
+    var normalIndex = IntArray(3) { 0 }
+    var texCoordIndex = IntArray(3) { 0 }
+}
 
 interface Mesh {
-
+    fun verticesCount(): Long
+    fun trianglesCount(): Long
 }
 
 class ObjMesh : Mesh {
+    var _vertices: MutableList<Vector3> = mutableListOf()
+    var _normals: MutableList<Vector3> = mutableListOf()
+    var _texcrds: MutableList<Vector2> = mutableListOf()
+    var _vertclr: MutableList<Vector3> = mutableListOf()
+    var _triangles: MutableList<ObjTriangleIndices> = mutableListOf()
+    var _name: String = ""
+    var _bounding: AABoundingBox = AABoundingBox()
 
+    fun verticesData(): List<Vector3> = _vertices
+    fun indicesData(): List<ObjTriangleIndices> = _triangles
+    fun normalsData(): List<Vector3> = _normals
+    fun texcrdsData(): List<Vector2> = _texcrds
+    fun vertclrData(): List<Vector3> = _vertclr
+
+    fun vertices(): List<Vector3> = _vertices
+    fun indices(): List<ObjTriangleIndices> = _triangles
+    fun normals(): List<Vector3> = _normals
+    fun texcrds(): List<Vector2> = _texcrds
+    fun vertclr(): List<Vector3> = _vertclr
+
+    fun setVertices(vertices: List<Vector3>) {
+        _vertices = vertices.toMutableList()
+    }
+
+    fun setTriangles(triangles: List<ObjTriangleIndices>) {
+        _triangles = triangles.toMutableList()
+    }
+
+    fun setNormals(normals: List<Vector3>) {
+        _normals = normals.toMutableList()
+    }
+
+    fun setTexCoords(texCoords: List<Vector2>) {
+        _texcrds = texCoords.toMutableList()
+    }
+
+    fun setVertexColors(vertexColors: List<Vector3>) {
+        _vertclr = vertexColors.toMutableList()
+    }
+
+    fun bounding(): AABoundingBox = _bounding
+
+    fun setBounding(bounding: AABoundingBox) {
+        _bounding = bounding
+    }
+
+    fun name(): String = _name
+
+    fun setName(name: String) {
+        _name = name
+    }
+
+    override fun verticesCount(): Long = _triangles.size * 3L
+    override fun trianglesCount(): Long = _triangles.size.toLong()
 }
 
 class InterleavedMesh : Mesh {
+    data class Vertex(
+        var pos: Vector3,
+        var norm: Vector3,
+        var uv: Vector2
+    )
 
+    var _vertices: MutableList<Vertex> = mutableListOf()
+    var _name: String = ""
+    var _bounding: AABoundingBox = AABoundingBox()
+
+    constructor() {}
+
+    constructor(other: ObjMesh) {
+        _name = other._name
+        _bounding = other._bounding
+        for (indecies in other._triangles) {
+            val v1 = other._vertices[indecies.vertexIndex[0]]
+            val v2 = other._vertices[indecies.vertexIndex[1]]
+            val v3 = other._vertices[indecies.vertexIndex[2]]
+
+            val n1 = other._normals[indecies.normalIndex[0]]
+            val n2 = other._normals[indecies.normalIndex[1]]
+            val n3 = other._normals[indecies.normalIndex[2]]
+
+            val tx1 = other._texcrds[indecies.texCoordIndex[0]]
+            val tx2 = other._texcrds[indecies.texCoordIndex[1]]
+            val tx3 = other._texcrds[indecies.texCoordIndex[2]]
+
+            val vertex1 = Vertex(v1, n1, tx1)
+            val vertex2 = Vertex(v2, n2, tx2)
+            val vertex3 = Vertex(v3, n3, tx3)
+
+            _vertices.add(vertex1)
+            _vertices.add(vertex2)
+            _vertices.add(vertex3)
+        }
+    }
+
+    fun bounding(): AABoundingBox = _bounding
+
+    fun setBounding(bounding: AABoundingBox) {
+        _bounding = bounding
+    }
+
+    fun name(): String = _name
+
+    fun setName(name: String) {
+        _name = name
+    }
+
+    override fun verticesCount(): Long = _vertices.size.toLong()
+    override fun trianglesCount(): Long = _vertices.size / 3L
 }
 
 class SeparatedBuffersMesh : Mesh {
+    var _vertices: MutableList<Vector3> = mutableListOf()
+    var _normals: MutableList<Vector3> = mutableListOf()
+    var _texcrds: MutableList<Vector2> = mutableListOf()
+    var _name: String = ""
+    var _bounding: AABoundingBox = AABoundingBox()
 
+    constructor() {}
+
+    constructor(other: ObjMesh) {
+        _name = other._name
+        _bounding = other._bounding
+        for (indecies in other._triangles) {
+            val v1 = other._vertices[indecies.vertexIndex[0]]
+            val v2 = other._vertices[indecies.vertexIndex[1]]
+            val v3 = other._vertices[indecies.vertexIndex[2]]
+
+            val n1 = other._normals[indecies.normalIndex[0]]
+            val n2 = other._normals[indecies.normalIndex[1]]
+            val n3 = other._normals[indecies.normalIndex[2]]
+
+            val tx1 = other._texcrds[indecies.texCoordIndex[0]]
+            val tx2 = other._texcrds[indecies.texCoordIndex[1]]
+            val tx3 = other._texcrds[indecies.texCoordIndex[2]]
+
+            _vertices.add(v1)
+            _vertices.add(v2)
+            _vertices.add(v3)
+
+            _normals.add(n1)
+            _normals.add(n2)
+            _normals.add(n3)
+
+            _texcrds.add(tx1)
+            _texcrds.add(tx2)
+            _texcrds.add(tx3)
+        }
+    }
+
+    fun bounding(): AABoundingBox = _bounding
+
+    fun setBounding(bounding: AABoundingBox) {
+        _bounding = bounding
+    }
+
+    fun name(): String = _name
+
+    fun setName(name: String) {
+        _name = name
+    }
+
+    override fun verticesCount(): Long = _vertices.size.toLong()
+    override fun trianglesCount(): Long = _vertices.size / 3L
+}
+
+class AABoundingBox {
+    var min: Vector3 = Vector3()
+    var max: Vector3 = Vector3()
 }
--- modules/src/main/kotlin/mesh/Obj.kt
+++ modules/src/main/kotlin/mesh/Obj.kt
@@ -1,13 +1,196 @@
 package mesh
 
-import kotlin.io.path.Path
-import kotlin.io.path.exists
+import algebra.Vector2
+import algebra.Vector3
+import java.io.File
 import java.io.FileNotFoundException
 
-//fun reaadObjFile(path: String): Mesh {
-//    val file = Path(path)
-//    if (!file.exists()) {
-//        throw FileNotFoundException("object file not found at path \"${path}\"")
-//    }
-//    return Mesh()
-//}
+enum class ObjTokens {
+    GEOMETRIC_VERTICES,
+    TEXTURE_VERTICES,
+    VERTEX_NORMALS,
+    PARAMETER_SPACE_VERTICES,
+    CURVE_OR_SURFACE,
+    DEGREE,
+    BASIS_MATRIX,
+    STEP_SIZE,
+    POINT,
+    LINE,
+    FACE,
+    CURVE,
+    CURVE_2D,
+    SOURFACE,
+    PARAMETER_VALUES,
+    OUTER_TRIMMING_LOOP,
+    INNER_TRIMMING_LOOP,
+    SPECIAL_CURVE,
+    SPECIAL_POINT,
+    END_STATEMENT,
+    CONNECT,
+    GROUP_NAME,
+    SMOOTHING_GROUP,
+    MERGING_GROUP,
+    OBJECT_NAME,
+    BEVEL_INTERPOLATION,
+    COLOR_INTERPOLATION,
+    DISSOLVE_INTERPOLATION,
+    LEVEL_OF_DETAIL,
+    MATERIAL_NAME,
+    MATERIAL_LIBRARY,
+    SHADOW_CASTING,
+    RAY_TRACING,
+    CURVE_APPROXIMATION_TECHNIQUE,
+    SURFACE_APPROXIMATION_TECHNIQUE,
+    COMMENT,
+}
+
+val tokens_: Map<ObjTokens, String> = mapOf(
+    ObjTokens.GEOMETRIC_VERTICES to "v ",
+    ObjTokens.TEXTURE_VERTICES to "vt ",
+    ObjTokens.VERTEX_NORMALS to "vn ",
+    ObjTokens.PARAMETER_SPACE_VERTICES to "vp ",
+    ObjTokens.CURVE_OR_SURFACE to "cstype ",
+    ObjTokens.DEGREE to "deg ",
+    ObjTokens.BASIS_MATRIX to "bmat ",
+    ObjTokens.STEP_SIZE to "step ",
+    ObjTokens.POINT to "p ",
+    ObjTokens.LINE to "l ",
+    ObjTokens.FACE to "f ",
+    ObjTokens.CURVE to "curv ",
+    ObjTokens.CURVE_2D to "curv2 ",
+    ObjTokens.SOURFACE to "surf ",
+    ObjTokens.PARAMETER_VALUES to "parm ",
+    ObjTokens.OUTER_TRIMMING_LOOP to "trim ",
+    ObjTokens.INNER_TRIMMING_LOOP to "hole ",
+    ObjTokens.SPECIAL_CURVE to "scrv ",
+    ObjTokens.SPECIAL_POINT to "sp ",
+    ObjTokens.END_STATEMENT to "end ",
+    ObjTokens.CONNECT to "con ",
+    ObjTokens.GROUP_NAME to "g ",
+    ObjTokens.SMOOTHING_GROUP to "s ",
+    ObjTokens.MERGING_GROUP to "mg ",
+    ObjTokens.OBJECT_NAME to "o ",
+    ObjTokens.BEVEL_INTERPOLATION to "bevel ",
+    ObjTokens.COLOR_INTERPOLATION to "c_interp ",
+    ObjTokens.DISSOLVE_INTERPOLATION to "d_interp ",
+    ObjTokens.LEVEL_OF_DETAIL to "lod ",
+    ObjTokens.MATERIAL_NAME to "usemtl ",
+    ObjTokens.MATERIAL_LIBRARY to "mtllib ",
+    ObjTokens.SHADOW_CASTING to "shadow_obj ",
+    ObjTokens.RAY_TRACING to "trace_obj ",
+    ObjTokens.CURVE_APPROXIMATION_TECHNIQUE to "ctech ",
+    ObjTokens.SURFACE_APPROXIMATION_TECHNIQUE to "stech ",
+    ObjTokens.COMMENT to "# "
+)
+
+fun split(str: String, delim: Char): List<String> {
+    val result = mutableListOf<String>()
+    var left = 0
+    for (i in str.indices) {
+        if (str[i] == delim) {
+            result.add(str.substring(left, i))
+            left = i + 1
+        }
+    }
+    if (left < str.length) {
+        result.add(str.substring(left))
+    }
+    return result
+}
+
+fun parseVertexString(str: String): Triple<Float, Float, Float> {
+    val vertexValuesString = split(str, ' ')
+    val x = vertexValuesString[0].toFloat()
+    val y = vertexValuesString[1].toFloat()
+    val z = vertexValuesString[2].toFloat()
+    return Triple(x, y, z)
+}
+
+fun parseNormalString(str: String): Triple<Float, Float, Float> {
+    val normalValuesString = split(str, ' ')
+    val x = normalValuesString[0].toFloat()
+    val y = normalValuesString[1].toFloat()
+    val z = normalValuesString[2].toFloat()
+    return Triple(x, y, z)
+}
+
+fun parseTexCoordString(str: String): Pair<Float, Float> {
+    val texCoordValuesString = split(str, ' ')
+    val u = texCoordValuesString[0].toFloat()
+    val v = texCoordValuesString[1].toFloat()
+    return Pair(u, v)
+}
+
+fun parseTriangleString(str: String): ObjTriangleIndices {
+    val indicesString = split(str, ' ')
+
+    val triangle = ObjTriangleIndices()
+
+    var i = 0
+    for (indexString in indicesString) {
+        val indicies = split(indexString, '/')
+        val v = indicies[0].toInt()
+        val t = indicies[1].toInt()
+        val n = indicies[2].toInt()
+
+        triangle.vertexIndex[i] = v - 1
+        triangle.normalIndex[i] = n - 1
+        triangle.texCoordIndex[i] = t - 1
+
+        i++
+    }
+
+    return triangle
+}
+
+fun readWavefrontObjFile(filePath: String): SeparatedBuffersMesh {
+    val file = File(filePath)
+    if (!file.exists()) {
+        throw FileNotFoundException("File \"$filePath\" not exist!")
+    }
+
+    val fileHandle = file.readText()
+
+    val objMesh = ObjMesh()
+
+    for (line in fileHandle.lineSequence()) {
+        val str = line.trim()
+        if (str.isEmpty()) continue
+
+        when {
+            str.startsWith(tokens_[ObjTokens.COMMENT]!!) -> {
+                continue
+            }
+            str.startsWith(tokens_[ObjTokens.OBJECT_NAME]!!) -> {
+                objMesh._name = str.substring(tokens_[ObjTokens.OBJECT_NAME]!!.length).trim()
+                continue
+            }
+            str.startsWith(tokens_[ObjTokens.GEOMETRIC_VERTICES]!!) -> {
+                val vertexString = str.substring(tokens_[ObjTokens.GEOMETRIC_VERTICES]!!.length)
+                val (x, y, z) = parseVertexString(vertexString)
+                objMesh._vertices.add(Vector3(x, y, z))
+                continue
+            }
+            str.startsWith(tokens_[ObjTokens.VERTEX_NORMALS]!!) -> {
+                val normalString = str.substring(tokens_[ObjTokens.VERTEX_NORMALS]!!.length)
+                val (nx, ny, nz) = parseNormalString(normalString)
+                objMesh._normals.add(Vector3(nx, ny, nz))
+                continue
+            }
+            str.startsWith(tokens_[ObjTokens.TEXTURE_VERTICES]!!) -> {
+                val texcrdString = str.substring(tokens_[ObjTokens.TEXTURE_VERTICES]!!.length)
+                val (u, v) = parseTexCoordString(texcrdString)
+                objMesh._texcrds.add(Vector2(u, v))
+                continue
+            }
+            str.startsWith(tokens_[ObjTokens.FACE]!!) -> {
+                val triangleString = str.substring(tokens_[ObjTokens.FACE]!!.length)
+                val triangle = parseTriangleString(triangleString)
+                objMesh._triangles.add(triangle)
+                continue
+            }
+        }
+    }
+
+    return SeparatedBuffersMesh(objMesh)
+}

678177d82fff9f8d13929e7f36ebcf6590436a5c
Pavel Turapin <paveltrpn@mail.ru>
2026-08-15 10:11:02 +0400

    apply is used

--- modules/src/main/kotlin/render/Render.kt
+++ modules/src/main/kotlin/render/Render.kt
@@ -81,32 +81,36 @@ class Render : EventObserver {
         val TEXT_START_POS_Y = 26.0f
         val TEXT_PLANE_Z = 0.4f
 
-        val l1 = Label()
-        l1.text = "test string"
-        l1.position = Vector2(TEXT_START_POS_X + 0.0f, TEXT_START_POS_Y)
-        l1.z = TEXT_PLANE_Z
-        l1.letterSpace = 0.0f
-        l1.color = Color(255, 255, 255, 255)
+        val l1 = Label().apply {
+            text = "test string"
+            position = Vector2(TEXT_START_POS_X + 0.0f, TEXT_START_POS_Y)
+            z = TEXT_PLANE_Z
+            letterSpace = 0.0f
+            color = Color(255, 255, 255, 255)
+        }
 
-        val l2 = Label()
-        l2.text = "test string"
-        l2.position = Vector2(TEXT_START_POS_X + 0.0f, TEXT_START_POS_Y + 3.5f)
-        l2.z = TEXT_PLANE_Z
-        l2.letterSpace = 0.0f
-        l2.letterScale = 1.5f
-        l2.color = Color(128, 255, 128, 255)
+        val l2 = Label().apply {
+            text = "test string"
+            position = Vector2(TEXT_START_POS_X + 0.0f, TEXT_START_POS_Y + 3.5f)
+            z = TEXT_PLANE_Z
+            letterSpace = 0.0f
+            letterScale = 1.5f
+            color = Color(128, 255, 128, 255)
+        }
 
-        val b1 = Billboard()
-        b1.size = Vector2(10.0f, 10.0f)
-        b1.position = Vector2(-31.0f, 16.0f)
-        b1.z = TEXT_PLANE_Z
-        b1.color = Color(255, 200, 128, 128)
+        val b1 = Billboard().apply {
+            size = Vector2(10.0f, 10.0f)
+            position = Vector2(-31.0f, 16.0f)
+            z = TEXT_PLANE_Z
+            color = Color(255, 200, 128, 128)
+        }
 
-        val b2 = Billboard()
-        b2.size = Vector2(10.0f, 10.0f)
-        b2.position = Vector2(-31.0f, 30.0f)
-        b2.z = TEXT_PLANE_Z
-        b2.color = Color(128, 200, 255, 128)
+        val b2 = Billboard().apply {
+            size = Vector2(10.0f, 10.0f)
+            position = Vector2(-31.0f, 30.0f)
+            z = TEXT_PLANE_Z
+            color = Color(128, 200, 255, 128)
+        }
 
         _ui.add(listOf(l1, l2, b1, b2))
 

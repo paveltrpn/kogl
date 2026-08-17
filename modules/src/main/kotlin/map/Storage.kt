44fd77f38ae39c58eaa04aef65a37509ca16b6c1
Pavel Turapin <paveltrpn@mail.ru>
2026-08-17 10:17:15 +0400

    update

--- modules/src/main/kotlin/map/Storage.kt
+++ modules/src/main/kotlin/map/Storage.kt
@@ -1,10 +1,11 @@
 package map
 
 import mesh.*
+import java.io.File
 
 class Storage private constructor(storagePath: String) {
-    private val _bodyStorage: HashMap<String, Mesh>? = null
-    private val _materialStorage: HashMap<String, Material>? = null
+    private val _bodyStorage: Map<String, Mesh>
+//    private val _materialStorage: Map<String, Material>
 
     companion object {
         @Volatile
@@ -24,6 +25,35 @@ class Storage private constructor(storagePath: String) {
     }
 
     init {
+        val storageDir = File("$storagePath/assets/bodies")
+        if (!storageDir.exists() || !storageDir.isDirectory) {
+            throw RuntimeException("Wrong storage directory \"$storagePath\"!")
+        }
 
+        val objFiles = storageDir.listFiles { file ->
+            file.isFile && file.extension.equals("obj", ignoreCase = true)
+        } ?: throw RuntimeException("Wrong storage directory content \"$storagePath\"!")
+
+        if (objFiles.isEmpty()) {
+            throw RuntimeException("No \"obj\" file found in directory \"$storagePath\"!")
+        }
+
+        val bs: MutableMap<String, Mesh> = mutableMapOf()
+        for (file in objFiles) {
+            val nameWithoutExtension = file.nameWithoutExtension
+            val mesh = readWavefrontObjFile(file.absolutePath)
+            bs[nameWithoutExtension] = SeparatedArraysMesh(mesh)
+        }
+
+        _bodyStorage = bs
+    }
+
+    val bodyStorage: Map<String, Mesh>
+        get() {
+            return _bodyStorage
+        }
+
+    operator fun get(name: String): Mesh {
+        return _bodyStorage[name] ?: throw RuntimeException("No mesh named \"$name\" in storage!")
     }
 }

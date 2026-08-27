704d9381f49a4c10a94ac72686c19fa73796dd7f
Pavel Turapin <paveltrpn@mail.ru>
2026-08-27 11:38:40 +0400

    update

--- kogl/assets/shaders/gl_grid_VERTEX.glsl
+++ kogl/assets/shaders/gl_grid_VERTEX.glsl
@@ -2,13 +2,13 @@ uniform mat4 view_matrix;
 uniform mat4 model_matrix;
 
 uniform float scale;
-uniform float zOffset;
+uniform float yOffset;
 
 out vec3 vWorldPos;
 
 vec4 vertecies[6] = vec4[](
-        vec4(1.0, 0.0, -1.0, 1.0), vec4(1.0, 0.0, 1.0, 1.0), vec4(-1.0, 0.0, 1.0, 1.0),
-        vec4(-1.0, 0.0, 1.0, 1.0), vec4(-1.0, 0.0, -1.0, 1.0), vec4(1.0, 0.0, -1.0, 1.0));
+        vec4(0.5, 0.0, -0.5, 1.0), vec4(0.5, 0.0, 0.5, 1.0), vec4(-0.5, 0.0, 0.5, 1.0),
+        vec4(-0.5, 0.0, 0.5, 1.0), vec4(-0.5, 0.0, -0.5, 1.0), vec4(0.5, 0.0, -0.5, 1.0));
 
 void main() {
     mat4 planeScale = mat4(1.0);
@@ -16,10 +16,13 @@ void main() {
     planeScale[2][2] = scale;
 
     mat4 planeYOffset = mat4(1.0);
-    planeYOffset[3][2] = zOffset;
+    planeYOffset[3][1] = yOffset;
 
     vec4 worldPos = planeScale * planeYOffset * vertecies[gl_VertexID];
 
     vWorldPos = worldPos.xyz;
     gl_Position = view_matrix * model_matrix * worldPos;
 }
+
+
+
